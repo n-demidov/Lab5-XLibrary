658c6e5bbc02b775a234c5d2640b630a0fa745b6
n-demidov <you@example.com>
2016-06-13 11:33:36 +0400

    add comments

--- source code/Lab5-XLibrary/Lab5-XLibrary-ejb/src/main/java/edu/library/exceptions/db/PersistException.java
+++ source code/Lab5-XLibrary/Lab5-XLibrary-ejb/src/main/java/edu/library/exceptions/db/PersistException.java
@@ -2,6 +2,9 @@ package edu.library.exceptions.db;
 
 import edu.library.exceptions.AbstractLibraryException;
 
+/**
+ * Data access layer exception
+ */
 public class PersistException extends AbstractLibraryException
 {
     
